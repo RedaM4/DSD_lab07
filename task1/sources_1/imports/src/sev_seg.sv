33c31f3dc0f9e0b81a1634b78e5af4729a5a8b74
Raid-the-XPRT <raltamimi0001@stu.kau.edu.sa>
2024-12-01 03:57:39 -0800

    Final_Task1

--- task1/sources_1/imports/src/sev_seg.sv
+++ task1/sources_1/imports/src/sev_seg.sv
@@ -1,29 +1,35 @@
 module sev_seg_top(
+    input wire BTNC,
     input wire CLK100MHZ,    // using the same name as pin names
     input wire CPU_RESETN,   
     output wire CA, CB, CC, CD, CE, CF, CG, DP,
     output wire [7:0] AN,    
     input wire [15:0]SW
+    
 );
 logic reset_n;
 logic clk;
+logic myClk;
+logic out;
+logic [3:0]disp;
 assign reset_n = CPU_RESETN;
 assign clk = CLK100MHZ;
+assign myClk=BTNC;
+
 
 wire [6:0] Seg;
 wire [3:0] digits[0:7];
-wire [3:0] signals;
 
 
-assign signals = SW[3:0] +1;
+sequenceDetector_Woverlap  sd1(.clk(myClk),.reset(reset_n),.in_bit(SW[0]),.detected(out));
 
+assign disp={1'b0,1'b0,1'b0,out};
 
-
-assign digits[0] = SW[3:0];
+assign digits[0] = out;
 assign digits[1] = 4'b0000;
 assign digits[2] = 4'b0000;
 assign digits[3] = 4'b0000;
-assign digits[4] = signals;
+assign digits[4] = 4'b0000;
 assign digits[5] = 4'b0000;
 assign digits[6] = 4'b0000;
 assign digits[7] = 4'b0000;
